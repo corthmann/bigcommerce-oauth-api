141aae5ed3bcc7fb76ecf5b1b11b1c8a44f596d3
Christian Orthmann <christian.orthmann@hotmail.com>
2014-10-28 00:57:19 +0100

    correct raising of errors

--- lib/faraday/raise_http_exception.rb
+++ lib/faraday/raise_http_exception.rb
@@ -6,13 +6,13 @@ module FaradayMiddleware
       @app.call(env).on_complete do |response|
         case response[:status].to_i
         when 400
-          raise BigcommerceOAuthAPI::BadRequest.new(response[:message])
+          raise BigcommerceOAuthAPI::BadRequest, response[:message]
         when 404
-          raise BigcommerceOAuthAPI::NotFound.new(response[:message])
+          raise BigcommerceOAuthAPI::NotFound, response[:message]
         when 500
-          raise BigcommerceOAuthAPI::InternalServerError.new(response[:message])
+          raise BigcommerceOAuthAPI::InternalServerError, response[:message]
         when 502
-          raise BigcommerceOAuthAPI::BadGateway.new(response[:message])
+          raise BigcommerceOAuthAPI::BadGateway, response[:message]
         end
       end
     end
